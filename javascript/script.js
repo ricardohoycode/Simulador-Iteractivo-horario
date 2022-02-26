a1f5aa32151dbcbdd3e20344c0c97c9eac40362c
Ricardo Hoyos <94667856+ricardohoycode@users.noreply.github.com>
2022-02-26 01:25:37 -0500

    Horario de Alaska-Reloj

---
+++ javascript/script.js
@@ -0,0 +1,21 @@
+(function() {
+
+     //Obtener fecha
+     var fecha = new Date();
+     document.write(fecha);
+
+     //Obtener horas
+     var hora = (fecha.getHours() - 12); 
+     var minutos = fecha.getMinutes(); 
+     var segundos = fecha.getSeconds(); 
+
+     console.log (); //ver resultado
+
+     //Obtener fechas
+     var dia = fecha.getDay();
+     var diames = fecha.getDate();
+     var mes = fecha.getMonth();
+     var year = fecha.getFullYear();
+
+
+}());
